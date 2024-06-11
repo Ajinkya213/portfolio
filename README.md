8a518f1201b5b82205f74a6cac3fd4239a2c10ad
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-06-11 11:51:41 +0530

    project-scaffolding
    Added the structure of the project on which I am going to build my portfolio.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfolio 
+
+Hello all this is my portfolio using React + TailwindCSS
+
+This is to showcase my learning and developement of front-end library ReactJS. 
+
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      Here is the start of my portfolio
+      This is the scaffolding of my project.
+    </>
+  )
+}
+
+export default App
