2061e6b4cf1d50e0192e3a7d28b4850964bae7d8
Ajinkya Bhushan <ajinkyabhushan213@gmail.com>
2024-07-23 13:17:32 +0530

    Updated Hero and Nav
    Fixed the tailwind classes issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
 import Navbar from "./component/Navbar"
 import Hero from "./component/Hero"
+import { useEffect, useState } from "react"
+
 
 function App() {
+
+  //For media quries
+  const deviceWidth = {
+    768 : "md",
+    1024 : "lg",
+  }
+  const getDeviceWidth = (width) => {
+    if (width >= 1024) return deviceWidth[1024];
+    if (width >= 768) return deviceWidth[768];
+    return "sm";
+  };
+
+  const { width } = window.screen;
+  const [activeWidth, setActiveWidth] = useState(width);
+
+  const [currentDeviceWidth, setCurrentDeviceWidth] = useState(getDeviceWidth(width));
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newWidth = window.innerWidth;
+      setActiveWidth(newWidth);
+      setCurrentDeviceWidth(getDeviceWidth(newWidth));
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+  }, [activeWidth]);
+
   return (
     <>
       <Navbar/>
-      <Hero/>
+      <Hero deviceWidth={currentDeviceWidth}/>
     </>
   )
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,14 @@
 
 const Navbar = () =>{
+
+  const links = ["About Me", "Tech Stack", "Projects"];
+
     return(
         <nav className="mt-5 flex justify-center">
-        <ul className="flex gap-4 rounded-full bg-white px-3 py-2 text-xs font-light shadow sm:text-xs md:px-4 md:text-sm">
-          <li className="rounded-full px-3 py-2 transition ease-in hover:ring hover:ring-green-500"><a href="">About Me</a></li>
-          <li className="rounded-full px-3 py-2 transition ease-in hover:ring hover:ring-green-500"><a href="">Tech Stack</a></li>
-          <li className="rounded-full px-3 py-2 transition ease-in hover:ring hover:ring-green-500"><a href="">Projects</a></li>
+        <ul className="flex gap-4 rounded-full bg-white px-3 py-2 text-sm font-light shadow sm:text-xs md:px-4 md:text-sm lg:text-base">
+          {
+            links.map(item => <li key={item} className="rounded-full px-3 py-2 transition ease-in hover:ring hover:ring-green-500"><a href="">{item}</a></li>)
+          }
         </ul>
       </nav>
     )
